2d03d37b5c59d9de87f0b670a3323bf214b15612
prasad123-hub <narkhedkarprasad@gmail.com>
2023-08-28 08:49:42 +0530

    Dialog, form, and Label components added

--- components/Forms/BankDetailsForm.tsx
+++ components/Forms/BankDetailsForm.tsx
@@ -2,13 +2,12 @@ import { useAppDispatch } from "@/hooks";
 import { nextStep } from "@/slices/StepSlice";
 import React from "react";
 
-export const PersonalDetailsForm = () => {
+export const BankDetailsForm = () => {
   const dispatch = useAppDispatch();
   return (
     <>
-      <div className="py-6">
-        <h2 className="text-2xl font-medium">Personal Details form</h2>
-      </div>
+      <h1>Bank details forms</h1>
+      <button onClick={() => dispatch(nextStep())}>Next</button>
     </>
   );
 };
--- components/Forms/KYCForms.tsx
+++ components/Forms/KYCForms.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Container } from "../Container";
 import { useAppSelector } from "@/hooks";
 import { PersonalDetailsForm } from "./PersonalDetailsForm";
+import { KYCDetailsForm } from "./KYCDetailsForm";
+import { BankDetailsForm } from "./BankDetailsForm";
 
 export const KYCForms = () => {
   const { currentStep } = useAppSelector((state) => state.step);
   return (
     <>
       <Container>
-        {currentStep == 1 ? <PersonalDetailsForm /> : <h1>Other</h1>}
+        {currentStep == 1 && <PersonalDetailsForm />}
+        {currentStep == 2 && <KYCDetailsForm />}
+        {currentStep == 3 && <BankDetailsForm />}
       </Container>
     </>
   );
